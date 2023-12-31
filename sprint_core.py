688d9a8afca858d5b22768ceab62a03f6b9fdad4
JUNG HOON PARK <pjh4139@gmail.com>
2023-12-31 21:00:13 +0900

    refer to README.md ( pushed at 2023-12-31 21:00:13

--- sprint_core.py
+++ sprint_core.py
@@ -1,20 +1,24 @@
+import inspect
 import os
 import string
 from datetime import datetime
 from typing import List
 
 from PySide6.QtCore import Signal, QThread
+from PySide6.QtWidgets import QApplication
 
-import pkg_park4139
-
-Park4139 = pkg_park4139.Park4139()
+from pkg_park4139 import UiUtil, Park4139
 
 directory_abspath = Park4139.PROJECT_DIRECTORY
 
 
 
+
+
+
 while 1:
-    Park4139.is_sync_directory_local(target_abspath=Park4139.PROJECT_DIRECTORY)
+    # Park4139.is_sync_directory_local(target_abspath=Park4139.PROJECT_DIRECTORY)
+    Park4139.monitor_target_edited_and_bkup(target_abspath=Park4139.PROJECT_DIRECTORY)
     Park4139.monitor_target_edited_and_bkup(target_abspath=Park4139.PARK4139_ARCHIVE_TOML)
 
 
